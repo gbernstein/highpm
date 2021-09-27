861b9325a85993f2457f9c41c7e7b33340b4323b
Gary Bernstein <garyb@physics.upenn.edu>
2021-09-27 13:57:11 -0400

    Fix various indexing errors

--- highpm/pmfit.py
+++ highpm/pmfit.py
@@ -20,43 +20,47 @@ def singleFit(xy,cov_xy, t,par_xy, parallax_prior=None):
     `chisq` vector of chisq of each observation.'''
 
     # Remove means from xy for numerical stability
-    xyMean = np.mean(xy, axis=1)
-    xy -= xyMean[:,np.newaxis]
+    xyMean = np.mean(xy, axis=0)
+    xy -= xyMean
     # Build matrices
-    npts = xy.shape[1]
+    npts = xy.shape[0]
     one = np.ones(npts, dtype=float)
     zero = np.zeros(npts, dtype=float)
 
     # Build 2 x N x 5 matrix of coefficients
-    A = np.array( [[one, zero, t, par_xy[:,0], zero],
-                   [zero, one, t, zero, par_xy[:,1]]])
+    A = np.array( [[one, zero, t, zero, par_xy[:,0]],
+                   [zero, one, zero, t, par_xy[:,1]]])
+    A = np.swapaxes(A,1,2)
+    
     # xy is the 2 x N "x" vector
     # Build 2 x 2 x N vector of inverse covariances:
     det = cov_xy[:,0]*cov_xy[:,1] - cov_xy[:,2]*cov_xy[:,2]
     invC = np.array( [[cov_xy[:,1], -cov_xy[:,2]],
                       [-cov_xy[:,2], cov_xy[:,0]]]) / det
 
+    print('shapes A, invC',A.shape,invC.shape,xy.shape) ###
     # Now the solution - contract over first two dimensions of A
-    alpha = np.einsum("ikm,ij,jkn",A,invC,A)
-    beta = np.einsum("ikm,ij,kj",A,invC,xy)
+    alpha = np.einsum("ikm,ijk,jkn",A,invC,A)
+    beta = np.einsum("ikm,ijk,kj",A,invC,xy)
     # Add parallax prior
     if parallax_prior is not None:
         alpha[4,4] += parallax_prior**-2
     # Solve for 5 parameters
-    p = np.solve(alpha, beta)
+    p = np.linalg.solve(alpha, beta)
 
     # Calculate fit and chisq per point
-    fit =  np.dot(A,p)
+    fit =  np.dot(A,p).T
     resid = xy - fit
-    chisq = np.einsum("ik,ij,jk",resid,invC,resid)
+    chisq = np.einsum("ki,ijk,kj->k",resid,invC,resid)
 
     # Put mean back into position
-    p[:1] += xyMean
+    p[:2] += xyMean
 
     return p, fit, chisq, alpha
 
                    
-def fit5d(cat, indices, chisqClip=11., parallax_prior=0.15, mjd_ref=57388.0 ):
+def fit5d(cat, indices, chisqClip=11., parallax_prior=0.15, mjd_ref=57388.0,
+              err_floor = 0.01):
     '''Execute 5d fit, with outlier rejection, on entries
     in the catalog at the rows specified by `indices`.
     Input catalog units are degrees but all parallax
@@ -69,6 +73,7 @@ def fit5d(cat, indices, chisqClip=11., parallax_prior=0.15, mjd_ref=57388.0 ):
         for a 2d measurement (default value is p~0.004).
     `parallax_prior`: sigma for prior on parallax
     `mjd_ref` is epoch date for positions, defaults to 2016.0
+    `err_floor` is sigma of additional (circular) error to add ###
     Returns:
     `p`: best-fit 5d parameters
     `alpha`: inverse-covariance matrix for fit
@@ -89,15 +94,17 @@ def fit5d(cat, indices, chisqClip=11., parallax_prior=0.15, mjd_ref=57388.0 ):
     # Put covariance into matrix form
     a = np.array(cat['ERRAWIN_WORLD'][indices]) * degree
     b = np.array(cat['ERRBWIN_WORLD'][indices]) * degree
+    a = np.hypot(a,err_floor)
+    b = np.hypot(b,err_floor)
     pa = np.array(cat['ERRTHETAWIN_J2000'][indices]) * np.pi / 180.  # in radians
     # Convert to cov
     ee = a*a - b*b
-    cov_xy = np.array( [[a*a+b*b + ee*np.cos(pa)],
-                        [a*a+b*b - ee*np.cos(pa)],
-                        [ee*np.sin(pa)]]) / 2.
-
+    cov_xy = np.array( [a*a+b*b + ee*np.cos(pa),
+                        a*a+b*b - ee*np.cos(pa),
+                        ee*np.sin(pa)]).T / 2.
     nClip = 0
     minPts = 4
+   
     # Begin fit/clip loop
     while xy.shape[0] >= minPts:
         p, fit, chisq, alpha = singleFit(xy, cov_xy, t, par_xy,
