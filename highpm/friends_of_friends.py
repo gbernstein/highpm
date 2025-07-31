5815eb64e1635ce2ee5533279f6e9f157f528f0d
vwetzell <vwetzell@sas.upenn.edu>
2025-07-31 08:29:38 -0400

    Removed astropy.fits and astropy.units. Docstrings are now reliable.

--- highpm/friends_of_friends.py
+++ highpm/friends_of_friends.py
@@ -1,6 +1,16 @@
+"""
+This module provides functions to find friends and groups of
+friends-of-friends in a a set of points, typically used in the context of
+spatial data analysis or social network analysis. The main functions include
+`find_friend` for finding neighbors within a specified distance and
+`friends_of_friends` for identifying connected groups based on direct and
+indirect friendships.
+"""
+
+
 def find_friend(data, length, cores=None):
-    """Finds friends within a specified distance using either a BallTree or parallel
-    query.
+    """Finds friends within a specified distance using either a BallTree or
+    parallel query.
 
     Parameters
     ----------
@@ -21,7 +31,7 @@ def find_friend(data, length, cores=None):
     If `cores` is None, uses `query_ball_tree` for neighbor search. Otherwise,
     uses `query_ball_point` with the specified number of workers.
     """
-    
+
     if cores is None:
         return data.query_ball_tree(data, length)
     else:
@@ -29,10 +39,10 @@ def find_friend(data, length, cores=None):
 
 
 def friends_of_friends(list_friends):
-    """Finds groups of friends-of-friends in a friendship network. Given a list where
-    each element contains the indices of direct friends for each person, this function
-    identifies all connected groups (including indirect connections via friends-of-
-    friends).
+    """Finds groups of friends-of-friends in a friendship network. Given a list
+    where each element contains the indices of direct friends for each person,
+    this function identifies all connected groups (including indirect
+    connections via friends-of-friends).
 
     Parameters
     ----------
@@ -44,14 +54,15 @@ def friends_of_friends(list_friends):
     -------
     result : list of list of int
         A list of groups, where each group is a list of indices representing
-        people who are all connected directly or indirectly through friendships.
+        people who are all connected directly or indirectly through
+        friendships.
     Examples
     --------
     >>> friends = [[1, 2], [0, 2], [0, 1, 3], [2]]
     >>> friends_of_friends(friends)
     [[0, 1, 2, 3]]
     """
-    
+
     todo = set(range(len(list_friends)))
     result = []
     while len(todo) > 0:
@@ -66,7 +77,7 @@ def friends_of_friends(list_friends):
                 fof = set(list_friends[next_friend])
                 todo.remove(next_friend)
                 fof = fof - new_set
-                fresh_friends = fresh_friends | fof 
+                fresh_friends = fresh_friends | fof
         result.append(list(new_set))
 
     return result
--- highpm/gnomonic_plate2sky.py
+++ highpm/gnomonic_plate2sky.py
@@ -1,7 +1,13 @@
+"""
+Convert gnomonic projection plate coordinates to sky coordinates (RA, Dec).
+"""
+
 import numpy as np
 
-def gnomonic_plate2sky(xi,eta,ra0,dec0):
-    """Convert gnomonic projection plate coordinates to sky coordinates (RA, Dec).
+
+def gnomonic_plate2sky(xi, eta, ra0, dec0):
+    """Convert gnomonic projection plate coordinates to sky coordinates
+    (RA, Dec).
 
     Parameters
     ----------
@@ -16,32 +22,38 @@ def gnomonic_plate2sky(xi,eta,ra0,dec0):
     Returns
     -------
     ra : float or ndarray
-        Right ascension(s) corresponding to the input plate coordinates, in degrees.
+        Right ascension(s) corresponding to the input plate coordinates, in
+        degrees.
     dec : float or ndarray
-        Declination(s) corresponding to the input plate coordinates, in degrees.
+        Declination(s) corresponding to the input plate coordinates, in
+        degrees.
     Notes
     -----
     This function assumes small-angle approximations are not used and works for
-    general positions on the sky. The input coordinates can be scalars or arrays.
+    general positions on the sky. The input coordinates can be scalars or
+    arrays.
     """
-    
 
-    xi_rad = xi * np.pi/180.
-    eta_rad = eta * np.pi/180.
+    xi_rad = xi * np.pi / 180.0
+    eta_rad = eta * np.pi / 180.0
 
-    ra0_rad = ra0 * np.pi/180.
-    dec0_rad = dec0 * np.pi/180.
+    ra0_rad = ra0 * np.pi / 180.0
+    dec0_rad = dec0 * np.pi / 180.0
 
-    rho = np.hypot(xi_rad,eta_rad)
+    rho = np.hypot(xi_rad, eta_rad)
     c = np.arctan(rho)
 
-    ra_rad = ra0_rad+np.arctan(xi_rad*np.sin(c)
-        /(rho*np.cos(dec0_rad)*np.cos(c)-eta_rad*np.sin(dec0_rad)*np.sin(c)))
+    ra_rad = ra0_rad + np.arctan(
+        xi_rad
+        * np.sin(c)
+        / (rho * np.cos(dec0_rad) * np.cos(c) - eta_rad * np.sin(dec0_rad) * np.sin(c))
+    )
 
-    dec_rad = np.arcsin(np.cos(c)*np.sin(dec0_rad)
-        +eta_rad*np.sin(c)*np.cos(dec0_rad)/rho)
+    dec_rad = np.arcsin(
+        np.cos(c) * np.sin(dec0_rad) + eta_rad * np.sin(c) * np.cos(dec0_rad) / rho
+    )
 
-    ra = ra_rad * 180./np.pi
-    dec = dec_rad * 180./np.pi
+    ra = ra_rad * 180.0 / np.pi
+    dec = dec_rad * 180.0 / np.pi
 
-    return ra,dec
+    return ra, dec
